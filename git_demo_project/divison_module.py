ca5dcbefd69b7218f84d3b4a210fbe52af8fe184
kareemkhan1258 <mohammed.kareem@ngenux.com>
2023-12-14 15:09:24 +0530

    added division_module.py

--- git_demo_project/divison_module.py
+++ git_demo_project/divison_module.py
@@ -1,6 +1,8 @@
 # below function performes division of first number with second number
 
-def divide_numbers(x,y):
-    #this function takes two numbers as inputs
-    divison = x/y    # x is divided with y and stored in divison
-    return divison   
+def divide_numbers(numbers):
+    result = numbers[0]
+    for i in numbers[1:]:
+        result = result/i
+    return result
+print(divide_numbers([2,3,4,5]))
