b095ca1780f819dc3705f4e3f5568f4e8974682c
sarmistha <sarmisthashaw97@gmail.com>
2023-12-14 10:32:34 +0530

    Created Calulation_module

--- git_demo_project/Calculation_module.py
+++ git_demo_project/Calculation_module.py
@@ -3,16 +3,12 @@ from calculation_module import calculate_result
 
 def calculate_result(operation_function, operand_numbers):
     if operation_function == add_numbers:
-        result_addition = calculate_result(add_numbers, operand_numbers)
         return ("Result of addition:", result_addition)
     if operation_function == subtract_numbers:
-        result_subtraction = calculate_result(subtract_numbers, operand_numbers)
         return("Result of subtraction:", result_subtraction)
     if operation_function == multiply_numbers:
-        result_multiplication = calculate_result(multiply_numbers, operand_numbers)
         return("Result of multiplication:", result_multiplication)
     if operation_function == divide_numbers:
-        result_division = calculate_result(divide_numbers, operand_numbers)
         return("Result of division:", result_division)
 
 
