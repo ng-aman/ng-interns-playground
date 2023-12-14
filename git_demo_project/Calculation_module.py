ec044297e15090725ec94c1fac472f08ea9bf0d6
sarmistha <sarmisthashaw97@gmail.com>
2023-12-14 17:34:59 +0530

    Created Calculate_result

--- git_demo_project/Calculation_module.py
+++ git_demo_project/Calculation_module.py
@@ -1,15 +1,11 @@
-from calculation_module import calculate_result
+def calculate_result(operation_function,numbers):
+    return operation_function(numbers)
 
 
-def calculate_result(operation_function, operand_numbers):
-    if operation_function == add_numbers:
-        return ("Result of addition:", result_addition)
-    if operation_function == subtract_numbers:
-        return("Result of subtraction:", result_subtraction)
-    if operation_function == multiply_numbers:
-        return("Result of multiplication:", result_multiplication)
-    if operation_function == divide_numbers:
-        return("Result of division:", result_division)
+
+
+
+
 
 
 
