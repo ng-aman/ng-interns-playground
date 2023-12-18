a9b15af8720918e5f8ec0c78bced14a0b7e007ae
sarmistha <sarmisthashaw97@gmail.com>
2023-12-18 17:10:53 +0530

    corrected the naming convensions exercise_1

--- submissions/s2/exercise_1.py
+++ submissions/s2/exercise_1.py
@@ -1,52 +1,52 @@
-class item:
+class Item:
     def __init__(self, name, price):
         self.name = name
         self.price = price
 
 
-class ORDER:
+class Order:
     def __init__(self):
-        self.its = []
+        self.items = []
 
-    def AddItem(self, item, quantity):
-        self.its.append({"i": item, "q": quantity})
+    def add_item(self, item, quantity):
+        self.items.append({"item": item, "quantity": quantity})
 
     def total(self):
         total = 0
-        for item in self.its:
-            total += item["i"].price * item["q"]
+        for item in self.items:
+            total += item["item"].price * item["quantity"]
         return total
 
 
-class billgenerator:
+class Billgenerator:
     def __init__(self, order):
         self.order = order
 
     def generate_bill(self):
         total_amount = self.order.total()
-        st = 0.05 * total_amount  # Assuming 5% service tax
+        service_tax = 0.05 * total_amount  # Assuming 5% service tax
         gst = 0.18 * total_amount  # Assuming 18% GST
 
-        final_amount = total_amount + st + gst
+        final_amount = total_amount + service_tax + gst
 
         # Display the bill
         print("------ Bill ------")
         for item in self.order.its:
-            print(f"{item['i'].name} x {item['q']}: {item['i'].price * item['q']}")
+            print(f"{item['item'].name} x {item['quantity']}: {item['item'].price * item['quantity']}")
 
         print(f"\nTotal Amount: {total_amount}")
-        print(f"Service Tax (5%): {st}")
+        print(f"Service Tax (5%): {service_tax}")
         print(f"GST (18%): {gst}")
         print(f"Final Amount: {final_amount}")
 
 
 # Example usage:
-item1 = item("item1", 10.0)
-item2 = item("item2", 15.0)
+item1 = Item("item1", 10.0)
+item2 = Item("item2", 15.0)
 
-order = ORDER()
-order.AddItem(item1, 2)
-order.AddItem(item2, 1)
+order = Order()
+order.add_item(item1, 2)
+order.add_item(item2, 1)
 
-bill_generator = billgenerator(order)
+bill_generator = Billgenerator(order)
 bill_generator.generate_bill()
